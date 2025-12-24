350ae8d1e4db8113574889370a0b3651a9e3fa0d
ishansasika <ishansasika@gmail.com>
2025-12-24 21:27:53 +0530

    feat: Add layout and global styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import '../app.css';
+  import Header from '$lib/components/Header.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<svelte:head>
+  <title>SvelteKit - Modern Svelte Template</title>
+</svelte:head>
+
+<div class="page-container">
+  <Header />
+  <div class="content-wrapper">
+    <slot />
+  </div>
+  <Footer />
+</div>
